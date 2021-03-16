31c8d996386680ae950496caffab35ccffdb5b1b
rish246 <rishabhkatna2228@gmail.com>
2021-03-16 11:48:46 +0530

    added k-fold cross validation

--- gender_classifier/TrainAndTest.py
+++ gender_classifier/TrainAndTest.py
@@ -31,15 +31,17 @@ def train_and_test_model(clf, X_train, y_train, X_test, y_test):
 
     ###################### print the accuracy of our classifier ###########################
     accuracy = metrics.accuracy_score(y_test, y_predicted, normalize = True)
-    print(f'Accuracy of our classifier is : {accuracy}')
+    # print(f'Accuracy of our classifier is : {accuracy}')
 
     ################# f-measure score #########################
     f_measure = metrics.f1_score(y_test, y_predicted, average='weighted', labels=np.unique(y_predicted))
-    print(f'F-Major of our classifier is : {f_measure}')
+    # print(f'F-Major of our classifier is : {f_measure}')
 
     ################# precesion ##############################
     precision = metrics.precision_score(y_test, y_predicted, average='weighted',labels=np.unique(y_predicted))
-    print(f'Precision of our classifier is {precision}')
+    # print(f'Precision of our classifier is {precision}')
     recall=metrics.recall_score(y_test, y_predicted, average='weighted')
-    print(f'Recall of our classifier is {recall}')
+    # print(f'Recall of our classifier is {recall}')
+
+    return accuracy, f_measure, precision, recall
    
