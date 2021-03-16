258efcec99236403f3226411ab06f800494fcd43
rish246 <rishabhkatna2228@gmail.com>
2021-03-16 16:17:10 +0530

    more refactor

--- gender_classifier/TrainAndTest.py
+++ gender_classifier/TrainAndTest.py
@@ -7,6 +7,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+from sklearn.model_selection import train_test_split, KFold
+
+
+def cross_validate_model(clf, X_train, X_test, y_train, y_test):
+    # we have extracted our features here in this function... Combine the X_train and X_test dataset
+    # form a giant 1 dataset
+    # cross validate that 1 giant dataset
+
+
+
+    whole_dataset_nparray = np.concatenate((X_train.toarray(), X_test.toarray()))
+
+    whole_output_nparray = y_train + y_test
+
+    kfold = KFold(n_splits = 10, shuffle = True, random_state = 1)
+    split_accuracies = []
+    split_f_measure = []
+    split_precision = []
+    split_recall = []
+
+    for train_ds_idxs, test_ds_idxs in kfold.split(whole_dataset_nparray):
+        X_train_cur, X_test_cur = whole_dataset_nparray[train_ds_idxs], whole_dataset_nparray[test_ds_idxs]
+        y_train = [whole_output_nparray[train_idx] for train_idx in train_ds_idxs]
+        y_test = [whole_output_nparray[test_idx] for test_idx in test_ds_idxs]
+
+
+
+        accuracy, f_measure, precision, recall = train_and_test_model(clf, X_train_cur, y_train, X_test_cur, y_test)
+
+        split_accuracies.append(accuracy)
+        split_f_measure.append(f_measure)
+        split_precision.append(precision)
+        split_recall.append(recall)
+
+
+    ### get avg accuracy
+    def get_avg(vals):
+        return (sum(vals) / len(vals))
+
+    average_accuracy = get_avg(split_accuracies)
+    average_f_measure = get_avg(split_f_measure)
+    average_precision = get_avg(split_precision)
+    average_recall = get_avg(split_recall)
+
+    return average_accuracy, average_f_measure, average_precision, average_recall
+
+
+
 def train_and_test_model(clf, X_train, y_train, X_test, y_test):
     # training phase
     # fit the X_train, y_train on the clf
@@ -20,13 +68,13 @@ def train_and_test_model(clf, X_train, y_train, X_test, y_test):
     '''
     Build the confusion matrix
     '''
-    confusion_matrix = metrics.confusion_matrix(y_test, y_predicted)
+    # confusion_matrix = metrics.confusion_matrix(y_test, y_predicted)
 
-    plt.imshow(confusion_matrix)
+    # plt.imshow(confusion_matrix)
 
-    plt.set_cmap('jet')
+    # plt.set_cmap('jet')
 
-    plt.show()
+    # plt.show()
 
 
     ###################### print the accuracy of our classifier ###########################
--- gender_classifier/main.py
+++ gender_classifier/main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 # sklearn libraries
 import numpy as np
-from numpy.core.fromnumeric import shape
-from numpy.core.numeric import cross
-from numpy.lib.function_base import average
-from numpy.lib.utils import who
-from sklearn.model_selection import train_test_split, KFold
+
+from sklearn.model_selection import train_test_split
 
 
     # skearn classifiers
@@ -33,7 +30,7 @@ from sklearn.svm import SVC, LinearSVC
 ########## from local modules
 from DataInput import load_data
 from FeatureExtraction import preprocess_tweet, extract_features
-from TrainAndTest import train_and_test_model
+from TrainAndTest import cross_validate_model
 
 
 ### global configurations #######
@@ -49,14 +46,6 @@ config2015= {
     'truth_path': '../data/pan15-author-profiling-training-dataset-english-2015-04-23/en/en.txt',
     'txts_destination_directory': '../data/pan15-author-profiling-training-dataset-english-2015-04-23/en',
 }
-
-
-
-
-
-############################################################################################
-####################3 Preprocess each tweet ###############################################
-
 
 
 
@@ -80,14 +69,8 @@ def generate_output(merged_tweets, truths, author_ids, original_tweet_lengths):
     X_train, X_test = extract_features(docs_train, docs_test, perform_dimensionality_reduction=False)
     print("Successfully extracted features from the documents")
 
-    # we have extracted our features here in this function... Combine the X_train and X_test dataset
-    # form a giant 1 dataset
-    # cross validate that 1 giant dataset
-    whole_dataset_nparray = np.concatenate((X_train.toarray(), X_test.toarray()))
-
-    whole_output_nparray = y_train + y_test
-
-    kfold = KFold(n_splits = 10, shuffle = False, random_state = 1)
+    
+    
 
     print("###############SVM###############")
     clf = LinearSVC(random_state = 42, tol=0.3)
@@ -96,33 +79,8 @@ def generate_output(merged_tweets, truths, author_ids, original_tweet_lengths):
 
     # clf = RandomForestClassifier(n_estimators=1000)
 
-
-    split_accuracies = []
-    split_f_measure = []
-    split_precision = []
-    split_recall = []
-
-    for train_ds_idxs, test_ds_idxs in kfold.split(whole_dataset_nparray):
-        X_train_cur, X_test_cur = whole_dataset_nparray[train_ds_idxs], whole_dataset_nparray[test_ds_idxs]
-        y_train = [whole_output_nparray[train_idx] for train_idx in train_ds_idxs]
-        y_test = [whole_output_nparray[test_idx] for test_idx in test_ds_idxs]
-
-
-
-        accuracy, f_measure, precision, recall = train_and_test_model(clf, X_train_cur, y_train, X_test_cur, y_test)
-
-        split_accuracies.append(accuracy)
-        split_f_measure.append(f_measure)
-        split_precision.append(precision)
-        split_recall.append(recall)
-    ######################################################################
-    # building classifiers
-
-    ### get avg accuracy
-    average_accuracy = sum(split_accuracies) / len(split_accuracies)
-    average_f_measure = sum(split_f_measure) / len(split_f_measure)
-    average_precision = sum(split_precision) / len(split_precision)
-    average_recall = sum(split_recall) / len(split_recall)
+    average_accuracy, average_f_measure, average_precision, average_recall = cross_validate_model(clf, X_train, X_test, y_train, y_test)
+    
 
     print(f'Average accuracy of our classifier : {average_accuracy}')
     print(f'Average F-Measure of our classifier : {average_f_measure}')
